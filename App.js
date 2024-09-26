1948ae08f53f3d9804f1817690b75bf4d8e814b4
Rejawanul Haque <rejhaqtonmoy@gmail.com>
2024-09-26 21:06:11 +0600

    hh

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Food_View_Component from './Components/Food_View_Component';
+import Order_Table_Calculation from './Components/Order_Table_Calculation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Food_View_Component />
+      <Order_Table_Calculation />
     </View>
   );
 }

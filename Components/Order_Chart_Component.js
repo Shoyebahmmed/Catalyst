5d1a6707af22e0a03364998b8a72cf106e7b8438
Rejawanul Haque <rejhaqtonmoy@gmail.com>
2024-10-02 16:13:00 +0600

    t

---
+++ Components/Order_Chart_Component.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function Order_Chart_components() {
+  return (
+    <View style={styles.container}>
+      
+        <Food_View_Container />
+      
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  
+});
